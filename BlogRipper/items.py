0f944a7781938524e592a6a38ff3f56d2cf6cbc2
Benoit Blanchon <benoit.blanchon@gmail.com>
2013-12-09 20:07:22 +0100

    Attempt to get only the links instead of crawling each post

--- BlogRipper/items.py
+++ BlogRipper/items.py
@@ -3,3 +3,8 @@ from scrapy.item import Item, Field
 class BlogripperItem(Item):
     title = Field()
     url = Field()
+    
+    def __init__(self, title, url):
+        Item.__init__(self)
+        self['title'] = title
+        self['url'] = url
--- BlogRipper/spiders/ericlippert.py
+++ BlogRipper/spiders/ericlippert.py
@@ -4,20 +4,26 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from BlogRipper.items import BlogripperItem
 from scrapy.contrib.loader.processor import MapCompose, TakeFirst
 
+class MySgmlLinkExtractor(SgmlLinkExtractor):
+    
+    def __init__(self, allow):
+        SgmlLinkExtractor.__init__(self, allow=allow)
+        
+    def unknown_endtag(self, tag):
+        if self.scan_tag(tag):
+            self.current_link = None    
+
 class EriclippertSpider(CrawlSpider):
     name = 'ericlippert'
     allowed_domains = ['blogs.msdn.com']
     start_urls = ['http://blogs.msdn.com/b/ericlippert/']
+    link_extractor = MySgmlLinkExtractor(allow=r'/archive/\d{4}/\d{2}/\d{2}/')
 
     rules = (
-        Rule(SgmlLinkExtractor(allow=r'/archive/\d{4}/\d{2}/\d{2}/'), callback='parse_item'),
-        Rule(SgmlLinkExtractor(allow=r'/default.aspx\?PageIndex=\d+'), callback='parse'),
+        Rule(SgmlLinkExtractor(allow=r'/default.aspx\?PageIndex=\d+')),
     )
 
-    def parse_item(self, response):
-        l = XPathItemLoader(BlogripperItem(), response=response)
-        l.add_value('url', response.url)
-        l.default_input_processor = MapCompose(unicode.strip)
-        l.default_output_processor = TakeFirst()
-        l.add_xpath('title', "/html/head/title/text()")
-        return l.load_item() 
+    def parse(self, response):
+        CrawlSpider.parse(self, response)
+        links = self.link_extractor.extract_links(response)        
+        return [ BlogripperItem(l.text, l.url) for l in links ]
