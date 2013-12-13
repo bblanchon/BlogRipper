fe24d1fad96aa75554f435f0f0db6c12fe53b1ba
Benoit Blanchon <benoit.blanchon@gmail.com>
2013-12-13 12:52:54 +0100

    Simplified installation script

--- install_scrapy.sh
+++ install_scrapy.sh
@@ -5,9 +5,8 @@ set -eu
 
 sudo apt-get update
 
-# PIP
+# Install python developement libs
 sudo apt-get install python-setuptools python-dev libxml2-dev libxslt-dev libz-dev
-sudo easy_install pip
 
-# Scrapy
-sudo pip install scrapy 
+# Install Scrapy
+sudo easy_install scrapy 
